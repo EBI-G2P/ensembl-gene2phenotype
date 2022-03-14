3c3066a0c6070e82f2a6b5ff3814bf131af8d3bc
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2022-03-14 17:25:12 +0000

    Adding changes

--- sql/patch_104_105_d.sql
+++ sql/patch_104_105_d.sql
@@ -20,15 +20,17 @@ CREATE TABLE ontology_term (
   PRIMARY KEY (ontology_accession_id)
 ) ENGINE=INNODB; 
 
+
 CREATE TABLE disease_ontology_mapping (
   disease_ontology_mapping_id int(10) NOT NULL,
-  disease_id INT(10) NOT NULL,
+  disease_id INT(10) unsigned NOT NULL,
   ontology_accession_id INT(10) NOT NULL, 
   mapped_by_attrib set('437', '438', '439', '440', '441', '442', '443', '444') DEFAULT NULL,
   PRIMARY KEY (disease_ontology_mapping_id),
-  FOREIGN KEY (ontology_accession_id) REFERENCES ontology_term(ontology_accession_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  FOREIGN KEY (disease_id) REFERENCES disease(disease_id) ON UPDATE CASCADE ON DELETE CASCADE
+  FOREIGN KEY (ontology_accession_id) REFERENCES ontology_term (ontology_accession_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (disease_id) REFERENCES disease (disease_id) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=INNODB; 
+
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_104_105_d.sql|creating ontology_term and ontology_accession table'); 
