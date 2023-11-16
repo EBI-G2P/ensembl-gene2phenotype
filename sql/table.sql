d14e7d71d7504ddc9241cefbd3c9ad2c33e11885
dglemos <dlemos@ebi.ac.uk>
2023-11-16 11:35:50 +0000

    Add missing table to sql files

--- sql/table.sql
+++ sql/table.sql
@@ -141,6 +141,21 @@ CREATE TABLE `genomic_feature_disease_log` (
   KEY genomic_feature_disease_idx (genomic_feature_disease_id)
 ) ENGINE=INNODB;
 
+CREATE TABLE `genomic_feature_disease_log_deleted` (
+  genomic_feature_disease_log_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  genomic_feature_disease_id int(10) unsigned NOT NULL,
+  genomic_feature_id int(10) unsigned NOT NULL,
+  disease_id int(10) unsigned NOT NULL,
+  confidence_category_attrib set('31','32','33','34','35') DEFAULT NULL,
+  is_visible tinyint(1) unsigned NOT NULL DEFAULT '1',
+  panel_attrib tinyint(1) DEFAULT NULL,
+  created timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  user_id int(10) unsigned NOT NULL,
+  action varchar(128) NOT NULL,
+  PRIMARY KEY (genomic_feature_disease_log_id),
+  KEY genomic_feature_disease_idx (genomic_feature_disease_id)
+) ENGINE=INNODB;
+
 CREATE TABLE genomic_feature_disease_panel (
   genomic_feature_disease_panel_id int(10) unsigned NOT NULL AUTO_INCREMENT,
   genomic_feature_disease_id int(10) unsigned NOT NULL,
