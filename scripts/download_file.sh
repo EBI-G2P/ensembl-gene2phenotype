9de8ee2edbd16eb60c893cebe476f6d222a26c34
dglemos <dlemos@ebi.ac.uk>
2024-12-02 14:43:41 +0000

    Update script to generate gencc file

--- scripts/download_file.sh
+++ scripts/download_file.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
+root_directory=$1
 current_date=$(date +'%Y-%m-%d')
-root_directory="/nfs/production/flicek/ensembl/variation/G2P/GenCC_create"
+# root_directory="/nfs/production/flicek/ensembl/variation/G2P/GenCC_create"
+# root_directory="/hps/nobackup/flicek/ensembl/variation/dlemos/G2P/gencc_submission" # Testing
 
 final_directory="$root_directory/$current_date"
 if [ ! -d "$final_directory" ];
@@ -9,8 +11,12 @@ then
     mkdir -m 777 $final_directory
 fi
 
-links=("https://www.ebi.ac.uk/gene2phenotype/downloads/CancerG2P.csv.gz"  "https://www.ebi.ac.uk/gene2phenotype/downloads/CardiacG2P.csv.gz"
-"https://www.ebi.ac.uk/gene2phenotype/downloads/DDG2P.csv.gz"  "https://www.ebi.ac.uk/gene2phenotype/downloads/EyeG2P.csv.gz"  "https://www.ebi.ac.uk/gene2phenotype/downloads/SkinG2P.csv.gz")
+links=("https://www.ebi.ac.uk/gene2phenotype/downloads/CancerG2P.csv.gz"
+       "https://www.ebi.ac.uk/gene2phenotype/downloads/CardiacG2P.csv.gz"
+       "https://www.ebi.ac.uk/gene2phenotype/downloads/DDG2P.csv.gz"
+       "https://www.ebi.ac.uk/gene2phenotype/downloads/EyeG2P.csv.gz"
+       "https://www.ebi.ac.uk/gene2phenotype/downloads/SkinG2P.csv.gz"
+       "https://www.ebi.ac.uk/gene2phenotype/downloads/SkeletalG2P.csv.gz")
 
 
 for str in ${links[@]}; do
